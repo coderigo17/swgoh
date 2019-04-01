a0637e69b546fab00e083e68511aac02906742d5
coderigo17 <daboinsanchez@college.harvard.edu>
2019-04-01 11:05:32 -0400

    update with TB script

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # swgoh
 
 TW banner and GP tracking info for Unseen.
+
+For expected behavior, scripts should be piped to a file, as per:
+
+python3 script.py > filename.txt
+
--- scripts/guild_gp.py
+++ scripts/guild_gp.py
@@ -1,14 +1,12 @@
-"""Run in terminal as follows: python3 tw_banners.py > filename.txt"""
+"""Run in terminal as follows: python3 guild_gp.py > filename.txt"""
 
 import json, requests
+
+from tabulate import tabulate
 
 # Unseen swgoh.gg guild id is 1154. Change below value to test on other guilds
 guild_id = 1154
 
-banner_goal = 200
-
-# Arbitrary GP to reach banner goal
-t_gp = 2800000
 
 def main():
     # Submit a GET request to api for guild info
@@ -21,14 +19,18 @@ def main():
     # Convert response to JSON and format nicely (for dev purposes)
     json_data = json.dumps(res.json(), indent=4, separators=(',', ': '))
 
+    guild_gps = []
+
     # Print output (ideally piped to a file via command line)
     for player in json.loads(json_data)['players']:
         p_name = player['data']['name']
         p_gp = player['data']['galactic_power']
+        p_cgp = player['data']['character_galactic_power']
+        p_sgp = player['data']['ship_galactic_power']
 
-        p_banner_goal = (p_gp / t_gp) * banner_goal
+        guild_gps.append([p_name, p_cgp, p_sgp, p_gp])
 
-        print(f"Name: {p_name} \nGP: {p_gp}\nBanner goal: {p_banner_goal}\n")
+    print(tabulate(guild_gps, headers=['Player name', 'Character GP', 'Ship GP', 'Total GP']))
 
 if __name__ == '__main__':
     main()
